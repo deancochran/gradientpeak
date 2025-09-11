e3cd266e1b2173854b3f690a8a070d3bff6aaed4
Dean Cochran <charles.dean.cochran@outlook.com>
2025-09-11 11:36:18 -0400

    update

--- apps/native/lib/db/migrations/migrations.js
+++ apps/native/lib/db/migrations/migrations.js
@@ -1,13 +1,16 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
-import m0000 from "./0000_abandoned_quasimodo.sql";
-import m0001 from "./0001_flashy_xorn.sql";
-import journal from "./meta/_journal.json";
+import journal from './meta/_journal.json';
+import m0000 from './0000_abandoned_quasimodo.sql';
+import m0001 from './0001_flashy_xorn.sql';
+import m0002 from './0002_next_sharon_ventura.sql';
 
-export default {
-  journal,
-  migrations: {
-    m0000,
-    m0001,
-  },
-};
+  export default {
+    journal,
+    migrations: {
+      m0000,
+m0001,
+m0002
+    }
+  }
+  
